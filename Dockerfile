353483686ed6621ee1a0f13cd97955d2f1015773
LeanaNeuber <LeanaNeuber@github.com>
2020-11-08 22:16:06 +0100

    Adding frontend

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.6
 
-WORKDIR /app
+WORKDIR /sentimentAnalysis
 
 ENV FLASK_APP=app.py
 
--- app/app.py
+++ app/app.py
@@ -1,61 +1,86 @@
 from flask import Flask, request, render_template
 from redis import RedisError, StrictRedis
 import pickle
-
 import pandas as pd
 import numpy as np
+from sklearn import preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn import model_selection, svm
+from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
+from sklearn.utils.validation import column_or_1d
+from sklearn.pipeline import Pipeline
 
 app = Flask(__name__)
-
 # ML code for sentiment analysis, could also be in an extra class
 def train_model():
     # do something to train model etc.
-	dataset = pd.read_csv('data.csv')
-	vector = TfidfVectorizer(sublinear_tf=True)
-	tf_vector =  vector.fit(np.array(dataset.iloc[:, 1]).ravel().astype('U'))
-	X = tf_vector.transform(np.array(dataset.iloc[:, 1]).ravel())
-	y = np.array(dataset.iloc[:, 0]).ravel()
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=30)
-	model = LogisticRegression(solver='lbfgs')
-	model.fit(X_train, y_train)
+    print('###### Load dataset')
+    dataset = pd.read_csv('app/Sentiment_Reviews.csv',index_col=0)
+    X = dataset[['Reviews']]
+    y = dataset[['Sentiment']]
+    le = preprocessing.LabelEncoder()
+    le.fit(y)
+    y = (le.transform(y))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=38)
+    print('###### train model')
+    text_clf_svm = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer(use_idf=False)), ('clf-svm', SGDClassifier(loss='modified_huber', penalty='l2', alpha=0.001, random_state=42, max_iter=20))])
+    text_clf_svm = text_clf_svm.fit(X_train['Reviews'], y_train)
     # store model to redis with pickle
-    pickled_model = pickle.dumps(model)
+    print('###### store model to redis with pickle')
+    pickled_model = pickle.dumps(text_clf_svm)
 
     try:
+        print('###### redis client set start TEST')
+        redis_client.set('test','test_value')
+        print('###### redis client set end TEST')
+
+        print('###### redis client set start')
         redis_client.set('ml_model', pickled_model)
+        print('###### redis client set end')
+        return
     except RedisError:
-        status = "fail"
+        return "###### fail1"
 
 
 # Get sentiment for phrase for pretrained model
 def get_sentiment(phrase):
     # Get stored model from redis
     try:
-        unpacked_model = pickle.loads(redis_client.get('ml_model'))
-		#apply model on phrase
-		test_prediction_lr = unpacked_model.predict(phrase)
-		test_prediction 
-        return test_prediction
+        print('###### redis client')
+        client_value = redis_client.get('ml_model')
+        print('###### unpacked model')
+        unpacked_model = pickle.loads(client_value)
+        #apply model on phrase
+        print('###### apply model')
+        result = unpacked_model.predict([phrase])
+        return int(result)
     except RedisError:
-        return "fail"
+        return "###### fail2"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    train_model()
     if request.method == 'POST':
         phrase = request.form
         if phrase['form_type'] == 'get_sentiment':
-            
-			return get_sentiment(phrase['phrase'])
+            sent = get_sentiment(phrase['phrase'])
+            print(sent)
+            if sent == 1:
+                return render_template('index_neutral.html')
+            elif sent == 2:
+                return render_template('index_positive.html')
+            elif sent == 0:
+                return render_template('index_negative.html')
+
     return render_template('index.html')
 
 if __name__ == '__main__':
     redis_client = StrictRedis(host='redis', port=6379)
+    train_model()
     app.run(host='0.0.0.0')
 
 
